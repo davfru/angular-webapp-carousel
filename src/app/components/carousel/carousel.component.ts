cc34ee05727dacf6c19767f294826c18584aadff
davfru <davide.fruci@gmail.com>
2024-05-29 22:59:36 +0200

    feat: horizontal carousel

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -8,7 +8,7 @@ import { Component, Input, ElementRef, ViewChild, AfterViewInit, OnChanges } fro
 export class CarouselComponent implements AfterViewInit, OnChanges {
 
   @Input() 
-  imageData: string[] = []
+  imageData: string[] = [];
 
   @ViewChild('carousel') carousel!: ElementRef<HTMLDivElement>;
   @ViewChild('imageDisplay') imageDisplay!: ElementRef<HTMLDivElement>;
@@ -22,6 +22,8 @@ export class CarouselComponent implements AfterViewInit, OnChanges {
   private mouseX: number = 0;
   private mouseY: number = 0;
 
+  constructor() {}
+
   ngAfterViewInit() {
     this.setupCarousel();
     this.startAnimation();
@@ -33,10 +35,6 @@ export class CarouselComponent implements AfterViewInit, OnChanges {
     }
   }
 
-  trackByFn(index: any, item: any): any {
-    return index;  
-  }
-
   setupCarousel() {
     this.angleUnit = 360 / this.imageData.length;
     this.mouseX = this.mouseY = 0;
@@ -46,36 +44,33 @@ export class CarouselComponent implements AfterViewInit, OnChanges {
     const items = this.carousel.nativeElement.children;
 
     for (let i = 0; i < items.length; i++) {
-      
       const item = items[i] as HTMLDivElement;
       const itemAngle = this.angleUnit * i;
       const itemAngleRad = itemAngle * Math.PI / 180;
-      const ypos = Math.sin(itemAngleRad) * this.RADIUS;
-      const zpos = Math.cos(itemAngleRad) * this.RADIUS;
-      const ypos1 = Math.sin(itemAngleRad) * (this.RADIUS + this.ITEM_SHIFT);
-      const zpos1 = Math.cos(itemAngleRad) * (this.RADIUS + this.ITEM_SHIFT);
+      const xpos = Math.cos(itemAngleRad) * this.RADIUS;
+      const zpos = Math.sin(itemAngleRad) * this.RADIUS;
+      const xpos1 = Math.cos(itemAngleRad) * (this.RADIUS + this.ITEM_SHIFT);
+      const zpos1 = Math.sin(itemAngleRad) * (this.RADIUS + this.ITEM_SHIFT);
 
-      item.style.transform = `translateY(${ypos}px) translateZ(${zpos}px) rotateX(${-itemAngle}deg)`;
+      item.style.transform = `translateX(${xpos}px) translateZ(${zpos}px) rotateY(0deg)`;
 
       item.onmouseover = () => {
-        item.style.transform = `translateY(${ypos1}px) translateZ(${zpos1}px) rotateX(${-itemAngle}deg)`;
+        item.style.transform = `translateX(${xpos1}px) translateZ(${zpos1}px) rotateY(0deg)`;
       };
-      
+
       item.onmouseout = () => {
-        item.style.transform = `translateY(${ypos}px) translateZ(${zpos}px) rotateX(${-itemAngle}deg)`;
+        item.style.transform = `translateX(${xpos}px) translateZ(${zpos}px) rotateY(0deg)`;
       };
-      
     }
   }
 
   startAnimation() {
-
     cancelAnimationFrame(this.animId);
 
     const updateFrame = () => {
       this.rotateAngle += this.mouseY;
       this.viewAngle += (this.mouseX - this.viewAngle) * 0.05;
-      this.carousel.nativeElement.style.transform = `translateZ(-1500px) rotateY(${-this.viewAngle}deg) rotateX(${this.rotateAngle}deg)`;
+      this.carousel.nativeElement.style.transform = `translateZ(-1500px) rotateX(${this.viewAngle}deg) rotateY(${-this.rotateAngle}deg)`;
       this.animId = requestAnimationFrame(updateFrame);
     };
 
@@ -94,5 +89,9 @@ export class CarouselComponent implements AfterViewInit, OnChanges {
 
   closeImage() {
     this.imageDisplay.nativeElement.style.transform = 'scale(0.0, 0.0)';
+  }
+
+  trackByFn(index: number, item: string): any {
+    return index;
   }
 }
